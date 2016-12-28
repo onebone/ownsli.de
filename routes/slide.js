8bd9d4fe0e7c8576a2fe2ad8e1a42a3a706938ed
HelloWorld017 <deu07115@gmail.com>
2016-12-28 22:48:52 +0900

    Updated slide editor, view

--- routes/slide.js
+++ routes/slide.js
@@ -5,6 +5,15 @@ router.get('/', (req, res, next) => {
 	res.render('slide/list');
 });
 
+router.get('/view/:id', (req, res, next) => {
+	res.render('slide/view');
+});
+
+router.get('/present/:id', (req, res, next) => {
+	//TODO Render presentation
+	res.send('');
+});
+
 router.get('/edit/:id', (req, res, next) => {
 	//TODO Send presentation data
 	res.render('slide/editor');

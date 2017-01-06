17a38418f491779ef3b99acbc07db6213e58729c
onebone <jyc00410@gmail.com>
2017-01-06 16:23:11 +0900

    Added routing invitation

--- routes/document.js
+++ routes/document.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {DocumentManager} = require('../src/document');
 const {SessionManager} = require('../src/session');
+const Sync = require('../src/sync');
 
 router.get('/get', (req, res) => {
 	if(!req.session.token) return res.send('null');
@@ -34,6 +35,34 @@ router.get('/get', (req, res) => {
 
 		res.json(arr);
 	}).catch((err) => res.send('null'));
+});
+
+router.get('/invite', (req, res) => {
+	if(!req.session.token) return res.send('null');
+
+	const session = SessionManager.getSession(req.session.token);
+
+	const username = req.query.username;
+	if(typeof username !== 'string') return;
+
+	if(session.getGroup() !== null){
+		const group = Sync.getGroup(session.getGroup());
+		if(group){
+			group.getDocument().addInvitation(username);
+			res.json({
+				status: true,
+				error: false
+			})
+		}else{
+			res.json({
+				status: true,
+				error: true
+			})
+		}
+	}else return res.json({
+		status: true,
+		error: true
+	});
 });
 
 module.exports = router;

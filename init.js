63c43850adef136254f2a998f4fbd6df8eb28dd6
Khinenw <khinenw@khinenw.tk>
2017-03-12 15:10:53 +0900

    Updated to production, blame onebone

--- init.js
+++ init.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
+const utils = require('./src/utils');
 
-global.config = JSON.parse(fs.readFileSync('config.json'));
+if(!fs.existsSync('config.json')){
+	const defaultConfig = JSON.parse(fs.readFileSync('./resources/config.json'));
+	defaultConfig.sessionKey = utils.createToken();
+	fs.writeFileSync('config.json', JSON.stringify(defaultConfig, null, '\t'));
+}
+global.config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 global.devmode = process.env.NODE_ENV !== 'production';
 global.userCreationToken = Math.random().toString(36).toUpperCase().slice(2, 8);
 console.log(chalk.cyan('User Creation Token : ' + userCreationToken));
